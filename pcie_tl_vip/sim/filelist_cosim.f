3cc37999a20c9a74817b64c70652b3d8d58810e9
Beihang-yuting <2965455908@qq.com>
2026-08-09 00:55:59 +0800

    feat(vip): add CQ BAR route metadata

--- pcie_tl_vip/sim/filelist_cosim.f
+++ pcie_tl_vip/sim/filelist_cosim.f
@@ -65,6 +65,7 @@ vcs-tb/cosim_xrc_pkg.sv
 pcie_tl_vip/tests/pcie_tl_base_test.sv
 pcie_tl_vip/tests/pcie_tl_cosim_test.sv
 pcie_tl_vip/tests/pcie_tl_dpu_501x_profile_test.sv
+pcie_tl_vip/tests/pcie_tl_route_metadata_test.sv
 
 // ---- testbench top ----
 pcie_tl_vip/tests/pcie_tl_tb_top.sv
